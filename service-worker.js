b6f8670c2ebbadae20de0330933e944e9168c3f7
ClemarZabala <clemarzabala2@gmail.com>
2025-10-21 13:36:49 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,22 @@
-const CACHE_NAME = "control-partes-v2"; // ← nueva versión
-const urlsToCache = ["./","./index.html","./style.css","./script.js","./manifest.json"];
+// service-worker.js
+const CACHE_NAME = "control-partes-v3";
+const urlsToCache = ["./","./index.html","./style.css","./script.js?v=3","./manifest.json","./icon-192.png","./icon-512.png"];
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)).then(() => self.skipWaiting())
   );
 });
 
 self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
+    .then(() => self.clients.claim())
   );
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(res => res || fetch(event.request))
+    caches.match(event.request).then(res => res || fetch(event.request)).catch(() => caches.match("./index.html"))
   );
 });
-
