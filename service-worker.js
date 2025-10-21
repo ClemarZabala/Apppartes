5cbb96a7ac6bf927aa544a38d84547f1768cced5
ClemarZabala <clemarzabala2@gmail.com>
2025-10-21 14:05:42 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,34 @@
-// service-worker.js
-const CACHE_NAME = "control-partes-v3";
-const urlsToCache = ["./","./index.html","./style.css","./script.js?v=3","./manifest.json","./icon-192.png","./icon-512.png"];
+const CACHE_NAME = "control-partes-v4";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js?v=4",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)).then(() => self.skipWaiting())
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
   );
 });
 
 self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
-    .then(() => self.clients.claim())
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
   );
 });
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(res => res || fetch(event.request)).catch(() => caches.match("./index.html"))
+    caches.match(event.request)
+      .then(res => res || fetch(event.request))
+      .catch(() => caches.match("./index.html"))
   );
 });
